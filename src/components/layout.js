9f4de98622c42b9e3545b3b6aa78ed8417990757
Walker Frederick <walker@urdev.xyz>
2019-09-24 16:29:23 -0400

    added navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,43 +5,96 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import HamburgerMenu from 'react-hamburger-menu'
+import oralignLogo from '../images/oralignLogo.svg'
 
-import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
+    query {
       site {
         siteMetadata {
           title
         }
       }
+      allContentfulAnnouncement {
+        nodes {
+          show,
+          message,
+          link
+        }
+      }
+      allContentfulNavigation {
+        nodes {
+          name,
+          items {
+            nav {
+              title,
+              nesting,
+              path,
+              nested {
+                title,
+                path
+              }
+            }
+          }
+        }
+      }
     }
   `)
 
+  const [state, setState] = useState({open: false})
+
+
+  const announcement = data.allContentfulAnnouncement.nodes[0]
+  const navigation = data.allContentfulNavigation.nodes[0].items.nav
+
+  const handleClick= function() {
+    setState({
+        open: !state.open
+    });
+}
+
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+    <div className="navigation box-shadow">
+      <nav>
+        {announcement.show ? <div className="announcment"><a target="_Blank" href={announcement.link}>{announcement.message}</a></div> : <></>}
+        <nav className="main-navigation">
+          <div className="space"></div>
+          <img src={oralignLogo}></img>
+          <div className="space">
+            <HamburgerMenu
+                isOpen={state.open}
+                menuClicked={handleClick.bind(this)}
+                width={30}
+                height={20}
+                strokeWidth={3}
+                rotate={0}
+                color='blue'
+                borderRadius={0}
+                animationDuration={0.5}
+            />
+          </div>
+        </nav>
+        <nav className={`sub-navigation ${state.open ? `menu-open` : `menu-closed`}`}>
+          {navigation.map(item => {
+            if (item.nesting === false) {
+              return <Link className="navigation-item" to={ item.path }>{item.title}</Link>
+            }
+            else if (item.nesting === true) {
+
+            }
+          })}
+        </nav>
+      </nav>
+  
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+
+    </div>
   )
 }
 
