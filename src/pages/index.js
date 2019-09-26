3c90d8cd2c40baacb900efe889b54a52f529ee35
Walker Frederick <walker@urdev.xyz>
2019-09-26 18:31:55 -0400

    fix failing build

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,9 @@ export const query = graphql`
           description,
           price,
           image {
-            file
+            file {
+              url
+            }
           }
         }
       }
